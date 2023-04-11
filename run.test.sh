96d0d4b7dbd50d5729f64d246e9ec52442767a63
KK <13557830797@163.com>
2023-04-11 10:43:38 +0800

    任务调度

--- run.test.sh
+++ run.test.sh
@@ -8,15 +8,15 @@ fi
 #cd /usr/share/nginx/html && /usr/local/bin/php artisan admin:update  # 这个命令会重置菜单中文翻译
 cd /usr/share/nginx/html && /usr/local/bin/php artisan migrate
 cd /usr/share/nginx/html && /usr/local/bin/php artisan storage:link
-cd /usr/share/nginx/html && /usr/local/bin/php artisan l5-swagger:generate &
+#cd /usr/share/nginx/html && /usr/local/bin/php artisan l5-swagger:generate &
 
 # start php server
 echo "start php server"
 nohup php-fpm -R >>/tmp/php-fpm.log &
 
 # start horizon server
-echo "start horizon server"
-cd /usr/share/nginx/html && nohup /usr/local/bin/php artisan horizon >>/usr/share/nginx/html/storage/logs/horizon.log &
+#echo "start horizon server"
+#cd /usr/share/nginx/html && nohup /usr/local/bin/php artisan horizon >>/usr/share/nginx/html/storage/logs/horizon.log &
 
 # start queue work server
 echo "start queue work server"
