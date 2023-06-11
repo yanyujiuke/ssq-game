3f0484753362ecb7a020bb5b4bf20c923ce7be55
KK <13557830797@163.com>
2023-06-11 15:14:06 +0800

    1

--- run.test.sh
+++ run.test.sh
@@ -5,10 +5,10 @@ if [ $(which nginx) ]; then
 fi
 
 # run pre-install
-#cd /usr/share/nginx/html && /usr/local/bin/php artisan admin:update  # 这个命令会重置菜单中文翻译
-cd /usr/share/nginx/html && /usr/local/bin/php artisan migrate
-cd /usr/share/nginx/html && /usr/local/bin/php artisan storage:link
-#cd /usr/share/nginx/html && /usr/local/bin/php artisan l5-swagger:generate &
+#cd /usr/share/nginx/html && php artisan admin:update  # 这个命令会重置菜单中文翻译
+cd /usr/share/nginx/html && php artisan migrate
+cd /usr/share/nginx/html && php artisan storage:link
+#cd /usr/share/nginx/html && php artisan l5-swagger:generate &
 
 # start php server
 echo "start php server"
@@ -16,11 +16,11 @@ nohup php-fpm -R >>/tmp/php-fpm.log &
 
 # start horizon server
 #echo "start horizon server"
-#cd /usr/share/nginx/html && nohup /usr/local/bin/php artisan horizon >>/usr/share/nginx/html/storage/logs/horizon.log &
+#cd /usr/share/nginx/html && nohup php artisan horizon >>/usr/share/nginx/html/storage/logs/horizon.log &
 
 # start queue work server
 echo "start queue work server"
-cd /usr/share/nginx/html && nohup /usr/local/bin/php artisan queue:work --queue=pay,loan,queue,push,default >>/usr/share/nginx/html/storage/logs/queue.log &
+cd /usr/share/nginx/html && nohup php artisan queue:work --queue=pay,loan,queue,push,default >>/usr/share/nginx/html/storage/logs/queue.log &
 
 # start cron schedule
 echo "start cron schedule"
@@ -28,7 +28,7 @@ log=/usr/share/nginx/html/storage/logs/cron.log
 result=""
 while true; do
     {
-        result=$(cd /usr/share/nginx/html && /usr/local/bin/php artisan schedule:run)
+        result=$(cd /usr/share/nginx/html && php artisan schedule:run)
         echo $result >>$log
     } &
     sleep 1m
