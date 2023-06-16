9a85d567d2373d0432931e6874377bc2f69fa218
KK <13557830797@163.com>
2023-06-16 13:59:28 +0800

    1

--- app/Console/Commands/TestCommand.php
+++ app/Console/Commands/TestCommand.php
@@ -5,6 +5,7 @@ namespace App\Console\Commands;
 use App\Jobs\BicolorSphereJob;
 use App\Jobs\SsqJob;
 use App\Lib\SsqLib;
+use App\Logic\Game\BicolorSphereLogic;
 use App\Logic\Game\SsqHistoryLogic;
 use Illuminate\Console\Command;
 
@@ -29,7 +30,24 @@ class TestCommand extends Command
      */
     public function handle()
     {
-        BicolorSphereJob::dispatch();
+        // 数据入库
+        $model = new BicolorSphereLogic();
+        $res = $model->findLast();
+        if (!$res || $res['date'] != date('Y-m-d')) {
+            $model->setData([
+                'date' => date('Y-m-d'),
+                'num' => isset($res['num']) ? $res['num'] + 1 : 0,
+                'red1' => 0,
+                'red2' => 1,
+                'red3' => 2,
+                'red4' => 3,
+                'red5' => 4,
+                'red6' => 5,
+                'blue' => 6,
+                'lottery_number' => '',
+            ]);
+        }
+        // BicolorSphereJob::dispatch();
         // SsqJob::dispatch();
         echo time() . PHP_EOL;
     }
